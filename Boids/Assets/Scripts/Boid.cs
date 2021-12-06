858b430642d8a0aac2d38543c889bb121698674e
DylanConvery <dylanconvery94@gmail.com>
2021-12-06 17:23:19 +0000

    wrote singleton pattern for BoidSpawner implemented boid velocity and direction logic so they now change direction depending on where their attraction is.

--- Boids/Assets/Scripts/Boid.cs
+++ Boids/Assets/Scripts/Boid.cs
@@ -6,19 +6,46 @@ public class Boid : MonoBehaviour
 {
     private void Awake()
     {
-        //coupling boid to boidspawner for the time being, will fix later
+        //TODO: decouple Boid and BoidSpawner logic
+        //grab rigidbody now to avoid having to getcomponent everywhere we need to access our rigidbody
         m_rigid_body = GetComponent<Rigidbody>();
-        m_position = Random.insideUnitSphere * BoidSpawner.spawner.boid_spawn_radius;
-        m_rigid_body.velocity = Random.onUnitSphere * BoidSpawner.spawner.boid_velocity;
+        //grab our attractions transform now for later
+        m_attraction_transform = GameObject.FindGameObjectWithTag("Attraction").transform;
+        //set the spawn position randomly within a 1.0f radius sphere * our spawn radius
+        m_position = Random.insideUnitSphere * BoidSpawner.boid_spawner.boid_spawn_radius;
+        //set the velocity of each boid to be random
+        m_rigid_body.velocity = Random.onUnitSphere * BoidSpawner.boid_spawner.boid_velocity;
         LookAhead();
     }
 
+    private void FixedUpdate()
+    {
+        Vector3 velocity = m_rigid_body.velocity;
+        //get current distance from attraction to boid
+        Vector3 delta = m_attraction_transform.position - m_position;
+
+        //check whether we need to move towards or away from the attraction
+        if (delta.magnitude > BoidSpawner.boid_spawner.attract_push_distance)
+        {
+            velocity = Vector3.Lerp(velocity, delta.normalized * BoidSpawner.boid_spawner.boid_velocity, BoidSpawner.boid_spawner.attraction_pull * Time.fixedDeltaTime);
+        }
+        else
+        {
+            velocity = Vector3.Lerp(velocity, -delta.normalized * BoidSpawner.boid_spawner.boid_velocity, BoidSpawner.boid_spawner.attraction_pull * Time.fixedDeltaTime);
+        }
+        //normalize vector and set velocity
+        m_rigid_body.velocity = velocity.normalized * BoidSpawner.boid_spawner.boid_velocity;
+        LookAhead();
+    }
+
+    //set our rotation to be in the direction of our velocity
     private void LookAhead()
     {
         transform.LookAt(m_position + m_rigid_body.velocity);
     }
 
     private Rigidbody m_rigid_body;
+    private Transform m_attraction_transform;
     private Vector3 m_position
     {
         get => transform.position;
--- Boids/Assets/Scripts/BoidSpawner.cs
+++ Boids/Assets/Scripts/BoidSpawner.cs
@@ -5,7 +5,15 @@ public class BoidSpawner : MonoBehaviour
 {
     void Awake()
     {
-        spawner = this;
+        //singleton
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
         SpawnBoids();
     }
 
@@ -28,13 +36,17 @@ public class BoidSpawner : MonoBehaviour
         m_boids.Add(boid);
     }
 
-    public static BoidSpawner spawner;
+    private static BoidSpawner _instance;
+    public static BoidSpawner boid_spawner => _instance;
 
     [SerializeField] private int m_boid_target_amount = 100;
     private const float m_creation_delay = 0.1f;
 
+    //boid spawn fields
     public float boid_spawn_radius = 100f;
     public float boid_velocity = 30f;
+    public float attract_push_distance = 5f;
+    public float attraction_pull = 2f;
 
     [SerializeField] private GameObject m_boid_prefab = null;
     [SerializeField] private GameObject m_boid_anchor = null;
